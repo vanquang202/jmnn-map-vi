0020c64a0da0fa8c04bf5c4ac3eb6296cf383189
vanquang202 <kopopi593@gmail.com>
2022-11-14 16:02:58 +0700

    Update repo

--- src/Commands/MapViCommand.php
+++ src/Commands/MapViCommand.php
@@ -3,7 +3,7 @@
 namespace Jmnn\MapVi\Commands;
 
 use Illuminate\Console\Command;
-use Str;
+use JMap;
 
 class MapViCommand extends Command
 {
@@ -11,20 +11,12 @@ class MapViCommand extends Command
 
     protected $description = 'Map vi';
 
-    public function __construct(private Str $str)
-    {
-        parent::__construct();
-    }
-
     public function handle()
     {
 
         $address = $this->__getData();
 
-        $result = array_filter($address,function ($data) {
-            $name = $this->__convertString($data['name']);
-            return (bool) $this->__macth($this->option('search'),$name);
-        });
+        $result = JMap::build($address,$this->option('search'));
 
         if(count($result) > 0)  $this->table(['Id','Name'], $result);
         else $this->table(['Id','Name'], [['Empty','Empty']]);
@@ -47,17 +39,5 @@ class MapViCommand extends Command
         return (array) json_decode((file_get_contents(__DIR__.'/data.json')),true);
     }
 
-    private function __convertString($data)
-    {
-        return $this->str::squish($this->str::slug($data,' '));
-    }
 
-    private function __macth($search , $str)
-    {
-        $patternString = implode(")|(",array_map(function ($data) {
-            return $this->__convertString($data);
-        },explode(",",$search)));
-        $pattern = '/('.$patternString.')/i';
-        return (bool) preg_match($pattern,$str);
-    }
 }
